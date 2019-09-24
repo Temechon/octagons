d01e6bcf01b8078370d34af272979a3f43d5f7d4
Julian <julian.chenard@gmail.com>
2019-09-24 22:29:00 +0200

    Best times

--- ts/states/Home.ts
+++ ts/states/Home.ts
@@ -16,7 +16,18 @@ module OCT {
             // g.destroy();
 
             // LOGO
-            let logo = this.game.add.sprite(this.game.width / 2 + 25 * ratio, 300 * ratio, 'logo');
+            let logo = this.game.add.sprite(this.game.width / 2 + 25 * ratio, 200 * ratio, 'logo');
+
+            let style = { font: Helpers.font(30, 'KeepCalm'), fill: "#BABABA", align: "center" };
+            // Levels done
+            let levelsDone = this.game.add.text(this.game.width / 2, logo.y + logo.height / 2 + 100 * ratio, "Levels done", style);
+            levelsDone.anchor.set(0.5);
+
+            // Number of levels
+            style.fill = "#A1A1A1";
+            style.font = Helpers.font(75, 'KeepCalm')
+            levelsDone = this.game.add.text(this.game.width / 2, levelsDone.y + levelsDone.height / 2 + 50 * ratio, "99", style);
+            levelsDone.anchor.set(0.5);
 
             let colors = [
                 "#FFBC67",
@@ -59,14 +70,36 @@ module OCT {
                 this.game.state.start('game', true, false, { difficulty: 3 });
             };
 
-            let style = { font: Helpers.font(40, 'KeepCalm'), fill: "#BABABA", align: "center" };
-            let levelsDone = this.game.add.text(this.game.width / 2, hardestbutton.y + hardestbutton.height / 2 + 150 * ratio, "Levels done", style);
-            levelsDone.anchor.set(0.5);
+            style.fill = "#BABABA";
+            style.font = Helpers.font(40, 'KeepCalm')
+            let bestTime = this.game.add.text(this.game.width / 2, hardestbutton.y + hardestbutton.height / 2 + 150 * ratio, "Best time", style);
+            bestTime.anchor.set(0.5);
 
             style.fill = "#A1A1A1";
-            style.font = Helpers.font(75, 'KeepCalm')
-            levelsDone = this.game.add.text(this.game.width / 2, levelsDone.y + levelsDone.height / 2 + 50 * ratio, "99", style);
-            levelsDone.anchor.set(0.5);
+            style.font = Helpers.font(40, 'KeepCalm');
+            let easy = this.game.add.text(this.game.world.centerX - 450 * ratio, bestTime.y + bestTime.height / 2 + 40 * ratio, "Easy", style);
+            easy.anchor.set(0.5);
+            let medium = this.game.add.text(this.game.world.centerX - 150 * ratio, easy.y, "Medium", style);
+            medium.anchor.set(0.5);
+            let hard = this.game.add.text(this.game.world.centerX + 150 * ratio, easy.y, "Hard", style);
+            hard.anchor.set(0.5);
+            let hardest = this.game.add.text(this.game.world.centerX + 450 * ratio, easy.y, "Hardest", style);
+            hardest.anchor.set(0.5);
+
+            style.font = Helpers.font(50, 'KeepCalm');
+            style.fill = "#FFBC67";
+            let easyTime = this.game.add.text(this.game.world.centerX - 450 * ratio, easy.y + 70 * ratio, "00:00", style);
+            easyTime.anchor.set(0.5);
+            style.fill = "#6DBCDB"
+            let mediumTime = this.game.add.text(this.game.world.centerX - 150 * ratio, easyTime.y, "00:00", style);
+            mediumTime.anchor.set(0.5);
+            style.fill = "#FC4349"
+            let hardTime = this.game.add.text(this.game.world.centerX + 150 * ratio, easyTime.y, "00:00", style);
+            hardTime.anchor.set(0.5);
+            style.fill = "#2C3E50"
+            let hardestTime = this.game.add.text(this.game.world.centerX + 450 * ratio, easyTime.y, "00:00", style);
+            hardestTime.anchor.set(0.5);
+
 
 
 
